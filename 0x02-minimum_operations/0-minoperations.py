2460ccb57afa0b7dfabbddce1a6d9ed76167247c
KawiraSharon <sharonkawira389@gmail.com>
2024-08-06 21:35:25 +0300

    Final bug fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
+"""minoperations"""
 
-
-''' A module that returns the minimum Operations it takes to
-    get to n characters.
-
-    Available operations:
-        copy
-        paste
-'''
 def minOperations(n):
-    """
-    minOperations
-    Gets fewest # of operations needed to result in exactly n H characters
-    """
-    # all outputs should be at least 2 char: (min, Copy All => Paste)
+    """Gets fewest # of operations needed to result in exactly n H characters"""
+    # outputs should be at least 2 characters
     if (n < 2):
         return 0
     ops, root = 0, 2
@@ -22,11 +12,10 @@ def minOperations(n):
         if n % root == 0:
             # total even-divisions by root = total operations
             ops += root
-            # set n to the remainder
+
             n = n / root
             # reduce root to find remaining smaller vals that evenly-divide n
             root -= 1
         # increment root until it evenly-divides n
         root += 1
     return ops
-    
